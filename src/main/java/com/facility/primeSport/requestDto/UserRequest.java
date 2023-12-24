5f8909b6b5a1dbd97ec9eaaa2da9531ffdfdac90
oktydeniz <oktydeniz07@gmail.com>
2023-12-24 22:44:07 +0300

    Fix bugs and give a desing and return pattern

--- src/main/java/com/facility/primeSport/requestDto/UserRequest.java
+++
@@ -1,9 +0,0 @@
-package com.facility.primeSport.requestDto;
-
-import lombok.Data;
-
-@Data
-public class UserRequest {
-    String email;
-    String password;
-}
--- src/main/java/com/facility/primeSport/response/UserResponse.java
+++
@@ -1,9 +0,0 @@
-package com.facility.primeSport.response;
-
-import lombok.*;
-
-public record UserResponse(
-    Long id,
-    String name,
-    String email
-){}
