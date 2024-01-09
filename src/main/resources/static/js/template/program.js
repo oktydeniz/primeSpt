07ab092c991fe0efdfe4cb4a95e91706db32ee10
oktydeniz <oktydeniz07@gmail.com>
2024-01-09 23:29:48 +0300

    Prime-1001- drag drop and swipe to delete flow added but only in html

--- src/main/resources/static/js/template/program.js
+++ src/main/resources/static/js/template/program.js
@@ -24,4 +24,38 @@ $(document).ready(function() {
             }
         });
     });
+
+    $(function() {
+
+        $(".container-cc").sortable({
+            items: ".swipeable", // Elements with this class will be sortable
+            handle: ".inner", // Use the ".inner" class as the handle to drag
+            axis: "y", // Only allow vertical dragging for reordering
+            containment: "parent", // Limit dragging within the parent container
+            connectWith: ".container-cc",
+            stop: function(event, ui) {
+                var newOrder = $(this).sortable("toArray");
+                console.log("New order:", newOrder);
+                // Perform any action with the new order here
+            }
+        });
+
+        $('.swipeable .inner').each(function(e) {
+            var snapper = new Snap({
+                element: this,
+                maxPosition: 100,
+                minPosition: -100,
+                disable: 'left'
+            });
+        });
+
+        $('.delete').on('click', function(e){
+            console.log("delete -clicked")
+            $(this).parent().slideToggle(100);
+        });
+
+        $('.save').on('click', function(e){
+            console.log("delete -save")
+        });
+    });
 });
